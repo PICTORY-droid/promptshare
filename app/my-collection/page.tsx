04d482a1ab70df9279039dec6c579a37b56a61d9
PICTORY-droid <oceanlumes@gmail.com>
2026-04-29 05:27:22 +0900

    my-collection getSession 추가

--- app/my-collection/page.tsx
+++ app/my-collection/page.tsx
@@ -56,6 +56,10 @@ export default function MyCollectionPage() {
   }, [])
 
   useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session?.user) { setUser(session.user); fetchPrompts(session.user.id) }
+      else { window.location.href = '/' }
+    })
     const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
       if (session?.user) {
         setUser(session.user)
