3c7dc7fbc75f13a0aee1820a789c0c7082f060d6
PICTORY-droid <oceanlumes@gmail.com>
2026-05-11 11:45:10 +0900

    chore: remove legacy prompt collection routes

--- app/my-collection/page.tsx
+++
@@ -1,4 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-const MyCollectionClient = dynamic(() => import('./client'), { ssr: false })
-export default function Page() { return <MyCollectionClient /> }
